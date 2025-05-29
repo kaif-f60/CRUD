9cc4d58efc6bb1ea521168cca56e1907ef14bd34
kaif-f60 <kaif@pentagaot.com>
2025-05-29 18:32:02 +0530

    add HTML code of Table

--- index.js
+++ index.js
@@ -2,39 +2,37 @@ const express = require ("express")
 const app = express()
 
 
+
+//Middleware
+
+app.set('view engine', 'ejs')
+app.use (express.urlencoded({extended:false}))
+app.use(express.static('public'))
+
+
+
+
 app.get('/',(req,res)=>{
-
-
+res.render('home')
 })
 
-app.get('/show-contact',(req,res)=>{
-
-    
+app.get('/show-contact',(req,res)=>{  
 })
 
 app.get('/add-contact',(req,res)=>{
-
-    
 })
 
 app.post('/add-contact',(req,res)=>{
-
-    
 })
 
 
 app.get('/update-contact',(req,res)=>{
-
-    
-})
-app.post('/update-contact',(req,res)=>{
-
-    
 })
 
-app.get('/delete-contact',(req,res)=>{
+app.post('/update-contact',(req,res)=>{ 
+})
 
-    
+app.get('/delete-contact',(req,res)=>{  
 })
 
 
