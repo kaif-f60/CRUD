0960fbc5f000cb76d2f8b4fe274a315722f1890c
kaif-f60 <kaif@pentagaot.com>
2025-05-29 17:52:05 +0530

    adding codes

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require ("express")
+const app = express()
+
+
+app.get('/',(req,res)=>{
+
+
+})
+
+app.get('/show-contact',(req,res)=>{
+
+    
+})
+
+app.get('/add-contact',(req,res)=>{
+
+    
+})
+
+app.post('/add-contact',(req,res)=>{
+
+    
+})
+
+
+app.get('/update-contact',(req,res)=>{
+
+    
+})
+app.post('/update-contact',(req,res)=>{
+
+    
+})
+
+app.get('/delete-contact',(req,res)=>{
+
+    
+})
+
+
+app.listen(3000, ()=>{console.log("SERVER IS WORKING NOW")})
