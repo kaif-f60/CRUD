c08661410fee1b5b93ee924b11946267b26f1334
kaif-f60 <kaif@pentagoat.com>
2025-09-04 14:43:57 +0530

    latest update

--- index.js
+++ index.js
@@ -10,27 +10,21 @@ app.use (express.urlencoded({extended:false}))
 app.use(express.static('public'))
 
 
+app.get('/',(req, res)=>{
 
-
-app.get('/',(req,res)=>{
-res.render('home')
+    const childs =[{name:"kailash", age:21},{name:"Kher", age:25},{name:"F60", age:25},]
+   res.json(
+    childs
+   );
 })
+
 
 app.get('/show-contact',(req,res)=>{ res.render('show-contact')})
-
 app.get('/add-contact', (req,res) =>{ res.render('add-contact')})
-
-app.post('/add-contact',(req,res)=>{
-})
-
-
+app.post('/add-contact',(req,res)=>{})
 app.get('/update-contact',(req,res)=>{ res.render('update-contact')})
-
-app.post('/update-contact',(req,res)=>{ 
-})
-
-app.get('/delete-contact',(req,res)=>{  
-})
+app.post('/update-contact',(req,res)=>{ })
+app.get('/delete-contact',(req,res)=>{ })
 
 
 app.listen(3000, ()=>{console.log("SERVER IS WORKING NOW")})
