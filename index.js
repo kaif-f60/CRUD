835befb1093f10d57ce2efb277ababe8566ebf52
kaif-f60 <kaif@pentagoat.com>
2025-10-31 15:15:48 +0530

    added code of update and delete

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/contact-crud')
 //Middleware
 
 app.set('view engine', 'ejs')
-app.use (express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
 
 
@@ -36,11 +36,32 @@ app.get('/show-contact/:id',async (req,res)=>{
     res.render('show-contact',{contact})
 
 })
-app.get('/add-contact', (req,res) =>{ res.render('add-contact')})
-app.post('/add-contact',(req,res)=>{})
-app.get('/update-contact/:id',(req,res)=>{ res.render('update-contact')})
-app.post('/update-contact/:id',(req,res)=>{ })
-app.get('/delete-contact/:id',(req,res)=>{ })
+app.get('/add-contact', (req,res) =>{ res.render('add-contact')
+})
+
+
+app.post('/add-contact', async(req,res)=>{
+    await Contact.create(req.body) 
+    res.redirect("/")})
+
+
+app.get('/update-contact/:id', async(req,res) => { 
+    const contact = await Contact.findById(req.params.id)
+    res.render('update-contact',{contact})   
+})
+
+app.post('/update-contact/:id', async(req,res)=>{
+
+await Contact.findByIdAndUpdate(req.params.id, req.body)
+    //
+res.redirect("/")
+ })
+app.get('/delete-contact/:id',async(req,res)=>{ 
+await Contact.findByIdAndDelete(req.params.id)
+res.redirect("/")   
+
+    
+})
 
 
 app.listen(3000, ()=>{console.log("SERVER IS WORKING NOW")})
